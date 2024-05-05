88e5bf12722b3bdfaeeeca499502623dd4cb379a
oiM-o <sugar_sugar2000@icloud.com>
2024-05-05 13:45:29 +0900

    room追加漏れ

--- app/src/main/java/com/github/oimo/apps/todoapp/TodoDatabase.kt
+++ app/src/main/java/com/github/oimo/apps/todoapp/TodoDatabase.kt
@@ -1,4 +1,18 @@
 package com.github.oimo.apps.todoapp
 
-class TodoDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.github.oimo.apps.todoapp.model.Todo
+import com.github.oimo.apps.todoapp.model.TodoDao
+
+@Database(
+    entities = [
+        Todo::class,
+    ],
+    version = 1,
+    exportSchema = true,
+)
+abstract class TodoDatabase: RoomDatabase() {
+
+    abstract fun todoDao(): TodoDao
 }
